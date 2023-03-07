0798a23d9b13225b6f93ce2487ceff27ec986626
iraklikutchiashvili <irakli9823@gmail.com>
2023-03-07 20:24:30 +0400

    Change font-size

--- src/components/SVGComponent.js
+++ src/components/SVGComponent.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 const SVGComponent = (props) => (
   <svg
-    width="40px"
-    height="40px"
+    width="20px"
+    height="20px"
     viewBox="0 0 48 48"
     fill="#21618c"
     xmlns="http://www.w3.org/2000/svg"
